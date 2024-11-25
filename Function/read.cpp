a01dfe753262b048991a3fc6cd609786d0063d69
Nguyen The Nhan (Timur) <156528501+TNT144cmd@users.noreply.github.com>
2024-11-26 00:32:20 +0700

    Update read.cpp

--- Function/read.cpp
+++ Function/read.cpp
@@ -27,6 +27,7 @@ void cache_read(unsigned int addr) {
 	if (way >= 0) {									// if we have a matching tag, then we have an data cache hit! (unless invalid MESI state)
 		
 		statistics.data_hit++;					// Incremenet the data hit counter (We have a new hit!)
+	//	L1_data[way][set].State = 'V'; 
 		L1_data[way][set].tag_bits = tag;
 		L1_data[way][set].set_bits = set;
 		L1_data[way][set].address = addr;
@@ -84,22 +85,22 @@ void cache_read(unsigned int addr) {
 		statistics.data_miss++;							// Increment the data cache miss counter
 
         // check empty way
-		for (int i = 0; (way < 0 && i < 8); ++i) {		// First, check if we have any empty lines
+		for (int i = 0; (way < 0 && i < 4); ++i) {		// First, check if we have any empty lines
 			if (L1_data[i][set].tag_bits == EMPTY_TAG) {	
 				way = i;								// return the way that has an empty line
-				empty_flag = 1;							// if we have an empty line, toggle the empty_flag to high
+				// empty_flag = 1;							// if we have an empty line, toggle the empty_flag to high
 			}
 		}
 
         // Have empty way
 		if (way >= 0) {									// if we have an empty line, place the read data in it
 			
-         // L1_data[way][set].MESI_char = 'E';			// and mark it exclusive, it's the only line with this data
+         	L1_data[way][set].State = 'V';			// and mark it Valid, it's the only line with this data
 			L1_data[way][set].tag_bits = tag;
 			L1_data[way][set].set_bits = set;
 			L1_data[way][set].address = addr;
 
-			L1_LRU(way, set, empty_flag, 'D');			// Update the data cache LRU order/count
+			L1_LRU(way, set, 'D');			// Update the data cache LRU order/count
 
 			if (mode == 1) {							// Simulating a cache read communication with L2
 				cout << " [Data] Read from L2: L1 cache miss, obtain data from L2 " << hex << addr << '\n' << endl;
@@ -124,12 +125,12 @@ void cache_read(unsigned int addr) {
 			//if (way < 0) {					//If we don't have any invalid lines, 
 			way = find_LRU(set, 'D');				// Find the LRU way in the data cache
 			if (way >= 0) {							// if we have a way that's 0, (LRU in the data cache, we use it)
-			//	L1_data[way][set].MESI_char = 'E';	// the data here will be new, and exclusive to this cache
+				L1_data[way][set].State = 'V';	// the data here will be new, and Valid to this cache
 				L1_data[way][set].tag_bits = tag;
 				L1_data[way][set].set_bits = set;
 				L1_data[way][set].address = addr;
 
-				L1_LRU(way, set, empty_flag, 'D');	// update the L1 data cache LRU bits
+				L1_LRU(way, set, 'D');	// update the L1 data cache LRU bits
 			}
 //		}
 
