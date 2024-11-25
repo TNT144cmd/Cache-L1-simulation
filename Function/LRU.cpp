4a216bdaf651f7f24ad8dd7a8dd70d74e0793ac8
Nguyen The Nhan (Timur) <156528501+TNT144cmd@users.noreply.github.com>
2024-11-26 00:33:50 +0700

    Update LRU.cpp

--- Function/LRU.cpp
+++ Function/LRU.cpp
@@ -1,4 +1,4 @@
-#include "header.h"
+ #include "header.h"
 
 /*
 	This function updates the LRU bits for the Data Cache  and the instruction caches
@@ -13,44 +13,48 @@
 	*/
 
 
-void L1_LRU(unsigned int way, unsigned int set, bool empty_flag,char which_cache) {
+void L1_LRU(unsigned int way, unsigned int set, char which_cache) {
 	switch (which_cache) {
 	case ('D'): 	// We're in the L1_data cache if which_chache is 'D' or 'd'
 		// If the way is empty, we use it and decrement every way less than this way.
-		
-		// mấy way sau đó cũng empty nên k cần xét. OK !!!!!!!?
-		if (empty_flag) {
-			for (int i = 0; i < way; ++i) {
-				L1_data[i][set].LRU_bits = --L1_data[i][set].LRU_bits; //pre-decrement the LRU bits
-			}
-		}
-		
-		// If a way is NOT empty, we compare LRU bits of the current set with the LRU bits of the other sets
-		else {
+		// if (empty_flag) {
 			for (int i = 0; i < 4; ++i) {
+				// L1_data[i][set].LRU_bits = --L1_data[i][set].LRU_bits; //pre-decrement the LRU bits
 				if (L1_data[way][set].LRU_bits <= L1_data[i][set].LRU_bits && L1_data[i][set].LRU_bits > 0){
 					--L1_data[i][set].LRU_bits;
 				}
 			}
-		}
+		// }
+		
+		// If a way is NOT empty, we compare LRU bits of the current set with the LRU bits of the other sets
+		// else {
+		// 	for (int i = 0; i < 4; ++i) {
+		// 		if (L1_data[way][set].LRU_bits <= L1_data[i][set].LRU_bits && L1_data[i][set].LRU_bits > 0){
+		// 			--L1_data[i][set].LRU_bits;
+		// 		}
+		// 	}
+		// }
 		L1_data[way][set].LRU_bits = 0x4;	// Set the current set to MRU 11 (0x7 in Hex)
 		break;
 		
 	case ('I'): 	// We're in the L1_instruction cache if which_chache is 'I' or 'i'
 		// If a way is empty, we use it and decrement every way less than this way
-		if (empty_flag) {
-			for (int i = 0; i < way; ++i) {
-				L1_inst[i][set].LRU_bits = --L1_inst[i][set].LRU_bits;
-			}
-		}
-		// If a way is NOT empty, we compare LRU bits of the current set with the LRU bits of the other sets
-		else {
+		// if (empty_flag) {
 			for (int i = 0; i < 2; ++i) {
+				// L1_inst[i][set].LRU_bits = --L1_inst[i][set].LRU_bits;
 				if (L1_inst[way][set].LRU_bits > L1_inst[i][set].LRU_bits && L1_inst[i][set].LRU_bits > 0) {
-					--L1_inst[i][set].LRU_bits;     //no need to do anything, keep the same order
+					--L1_inst[i][set].LRU_bits;     
 				}
 			}
-		}
+		// }
+		// If a way is NOT empty, we compare LRU bits of the current set with the LRU bits of the other sets
+		// else {
+		// 	for (int i = 0; i < 2; ++i) {
+		// 		if (L1_inst[way][set].LRU_bits > L1_inst[i][set].LRU_bits && L1_inst[i][set].LRU_bits > 0) {
+		// 			--L1_inst[i][set].LRU_bits;     //no need to do anything, keep the same order
+		// 		}
+		// 	}
+		// }
 		L1_inst[way][set].LRU_bits = 0x1; // Set the current set to MRU 1 (0x1 in Hex)
 
 		break;
